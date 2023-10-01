881f19f8c6c8302e307aa1c6ca4a81eb24d75c2e
Vasya Kohotiuk <141154677+vasyakohotyk@users.noreply.github.com>
2023-10-01 18:12:01 +0300

    Update EmailValidator.java

--- src/EmailValidator.java
+++ src/EmailValidator.java
@@ -1,8 +1,8 @@
 public class EmailValidator {
     public static void main(String[] args) {
-        System.out.println(validateEmail("test@example.com")); // true
-        System.out.println(validateEmail("invalid.email@com")); // false
-        System.out.println(validateEmail("user123@domain")); // false
+        System.out.println(validateEmail("test@example.com"));
+        System.out.println(validateEmail("invalid.email@com"));
+        System.out.println(validateEmail("user123@domain"));
     }
 
     public static boolean validateEmail(final String email) {
@@ -13,9 +13,7 @@ public class EmailValidator {
         int atIndex = email.indexOf('@');
         int dotIndex = email.indexOf('.', atIndex);
 
-        // Перевірка наявності "@" та "." відповідно
         if (atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < email.length() - 1) {
-            // Перевірка символів перед "@" та після "."
             if (isValidCharacters(email.substring(0, atIndex)) &&
                     isValidCharacters(email.substring(dotIndex + 1))) {
                 return true;
